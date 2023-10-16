d17eb1718027aa6703506d42758592fd5e1f5e13
Alena <andrievskayaalena22@gmail.com>
2023-10-16 14:07:26 +0500

    add requirements.txt

--- candler/urls.py
+++ candler/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("", include("main.urls")),
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
